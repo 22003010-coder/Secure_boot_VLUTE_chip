fb21deadb1365df860da597ad4b74e5cdf58b076
22003010-coder <22003010@st.vlute.edu.vn>
2026-08-20 19:57:08 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 
 `default_nettype none
 
-module tt_um_sercure_boot (
+module tt_um_secure_boot (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
