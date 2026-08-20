12183d4c9eda509f071ae02e9b5b527b24ffde81
22003010-coder <22003010@st.vlute.edu.vn>
2026-08-20 19:41:41 +0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -30,10 +30,6 @@ module tb ();
    
   // Replace tt_um_example with your module name:
   tt_um_sercure_boot  user_project (
-      `ifdef GL_TEST
-         .VPWR(VPWR),
-         .VGND(VGND),
-      `endif
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
